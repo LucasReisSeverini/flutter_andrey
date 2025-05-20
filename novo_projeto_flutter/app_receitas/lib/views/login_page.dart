567f686876c216df16d51c66951a46d61ba570a1
Lucas Severini <lucas_reiseverini@yahoo.com.br>
2025-05-20 19:51:56 -0300

    feat: iniciado tela de login e tela de receitas

---
+++ novo_projeto_flutter/app_receitas/lib/views/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//criando um statfull digite stlful
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ novo_projeto_flutter/app_receitas/lib/views/receitas_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//criando um state full
+class ReceitasPage extends StatefulWidget {
+  const ReceitasPage({super.key});
+
+  @override
+  State<ReceitasPage> createState() => _ReceitasPageState();
+}
+
+class _ReceitasPageState extends State<ReceitasPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
